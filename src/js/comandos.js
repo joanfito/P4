ca7cdbb1772fbd9cce2136fedc65fcb53edba39b
joanfito <joan.fito.2015@salleurl.edu>
2017-12-21 21:31:43 +0100

    razas y roles al json inicial

--- src/js/comandos.js
+++ src/js/comandos.js
@@ -7,16 +7,205 @@
  * Función para cargar el fichero nuevaPartida.json en la API
  */
  function cargaJSON() {
-   /*var ajaxASYNC = {
-    request: function (url){
-        var xhr = new XMLHttpRequest();
-        xhr.addEventListener("load", function () {
-          console.log(this.responseCode);
-        });
-        xhr.open("POST", url, true);
-        xhr.send();
+  var url = 'http://puigpedros.salleurl.edu/pwi/pac4/partida.php?token=0aee8310-0212-424d-b2b2-8e7771e4982d&slot=nueva';
+
+  //Creamos el archivo
+  var ficheroJSON = JSON.stringify({
+    "razas": [{
+      "nombre": "Altmer",
+      "habilidad": "Soberbia Altmer",
+      "descripcion": "La habilidad para las artes mágicas de los altmer no tiene rival. Es por eso que tienen +2 de daño mágico inicial."
+    }, {
+      "nombre": "Bosmer",
+      "habilidad": "Tiro de Valen",
+      "descripcion": "Los bosmer son los mejores arqueros de todo Tamriel. Antes de empezar el combate, es capaz de realizar un tiro con el arco que inflinge +1 de daño verdadero al enemigo."
+    }, {
+      "nombre": "Dunmer",
+      "habilidad": "Ira del Antepasado",
+      "descripcion": "Los dunmer, nativos de Morrowind, donde se alza la Montaña Roja, un enorme volcán, no le temen al fuego. Cada 10 turnos, se rodean durante un turno de fuego, que inflinge +4 de daño verdadero al enemigo cuándo ataca."
+    }, {
+      "nombre": "Orci",
+      "habilidad": "Herrero de Orsinium",
+      "descripcion": "Los orcos son los mayores herreros de toda Tamriel. Las armas y escudos comprados en la tienda son un 20% mejores."
+    }, {
+      "nombre": "Imperial",
+      "habilidad": "Astucia de Cyrodiil",
+      "descripcion": "Los imperiales, nativos de Cyrodiil, sobresalen sobre las demadas razas por su astucia. Por eso, son capaces de conseguir un 25% más de oro."
+    }, {
+      "nombre": "Nórdico",
+      "habilidad": "Fuerza de Skyrim",
+      "descripcion": "Los nordicos son una de las grandes razas guerreras de Tamriel. Su excelente habilidad para con la espada, les otorga un 20% más de daño si llevan equipados una espada y un escudo."
+    }, {
+      "nombre": "Bretón",
+      "habilidad": "Piel de dragón",
+      "descripcion": "Los bretones, habitantes de Roca Alta, son una raza mestiza entre altmer y humanos, tienen parte de la habilidad mágica de los altmer y una gran resistencia hacia la magia. Disponen de +2 de resistencia mágica y +1 de daño mágico adicional."
+    }, {
+     "nombre": "Guardia Rojo",
+      "habilidad": "Subida de adrenalina",
+      "descripcion": "Los guardia rojo, habitantes de Páramo del Martillo, son la mejor raza guerrera de toda Tamriel. Su gran envergadura les permite usar armaduras pesadas, por lo tanto, tienen +2 de armadura adicional, y su execelente habilidad con armas de gran envergadura, les otorga +3 de daño físico si no llevan escudo equipado."
+    }, {
+      "nombre": "Argoniano",
+      "habilidad": "Piel de Hist",
+      "descripcion": "Los argonianos, una raza con aspecto de reptiliano que habitan en la Ciénaga Negra son resistentes a casi cualquier enfermedad. Despues de cada combate, tienen un 33% de curarse un 25% de su vida restante."
+    }, {
+      "nombre": "Khajita",
+      "habilidad": "Sigilo",
+      "descripcion": "Los khajita son una raza de aspecto felino originaria de Elsweyr y conocida por ser excelentes ladrones, gracias a su sigilo y agilidad. Tienen un 10% de posibilidades de esquivar el ataque de un enemigo."
+    }],
+    "roles": [{
+      "nombre": "Colegio de Hibernalia",
+      "descripcion": "En el Colegio de Hibernalia se aprende todo lo relacionado con las artes arcanas. Sus miembros son hábiles magos. +3 de daño mágico adicional y -1 de armadura y resistencia mágica.",
+      "ataque": 3,
+      "armadura": -1,
+      "resistenciaMagica": -1
+    }, {
+      "nombre": "Hermandad Oscura",
+      "descripcion": "La Hermandad Oscura es el gremio de asesinos más mortífero de toda Tamriel. Sus miembros son despiadados asesinos. +3 de daño física adicional y -1 de armadura de resistencia mágica",
+      "ataque": 3,
+      "armadura": -1,
+      "resistenciaMagica": -1
+    }, {
+      "nombre": "Legión Imperial",
+      "descripcion": "En el ejercito del imperio, la Legión Imperial, se encuentran los mejores soldados del imperio. Sus miembros son resistentes gracias a sus excelentes armaduras. -2 de daño y +2 de armadura y resistencia mágica.",
+      "ataque": -2,
+      "armadura": 2,
+      "resistenciaMagica": 2
+    }],
+    "player": {
+      "nombre": "",
+      "raza": "",
+      "sexo": "",
+      "rol": "",
+      "vida": 10,
+      "nivel": 0,
+      "xp": 100,
+      "ataque": 0,
+      "tipoAtaque": "",
+      "armadura": 0,
+      "resistenciaMagica": 0,
+      "manoderecha": "garrot",
+      "manoizquierda": "",
+      "mochila": [],
+      "estadoPartida": {
+        "x": 3,
+        "y": 1,
+        "nivel": -2,
+        "direccion": 0
+      }
+    },
+    "enemigos": [{
+      "nombre": "",
+      "vida": 10,
+      "tipoAtaque": "fisico",
+      "ataque": 3,
+      "armadura": 2,
+      "resistenciaMagica": 2,
+      "xp": 20,
+      "img": "",
+      "objetos": []
+    },{
+      "nombre": "",
+      "vida": 10,
+      "tipoAtaque": "magico",
+      "ataque": 3,
+      "armadura": 2,
+      "resistenciaMagica": 2,
+      "xp": 20,
+      "img": "",
+      "objetos": []
+    }],
+    "objetos": [ {
+      "armas": [{
+        "nombre": "",
+        "img": "",
+        "ataque": 3,
+        "precio": 100
+      },{
+        "nombre": "",
+        "img": "",
+        "ataque": 4,
+        "precio": 150
+      }]
+    },{
+      "escudos": [{
+        "nombre": "",
+        "img": "",
+        "armadura": 2,
+        "precio": 100
+      },{"nombre": "",
+        "img": "",
+        "armadura": 3,
+        "precio": 200}]
+    },{
+      "armaduras": [{
+        "nombre": "",
+        "img": "",
+        "armadura": 4,
+        "resistenciaMagica": 2,
+        "precio": 500
+      },{"nombre": "",
+        "img": "",
+        "armadura": 2,
+        "resistenciaMagica": 4,
+        "precio": 500}]
+    },{
+      "pociones": [{
+        "nombre": "",
+        "img": "",
+        "curacion": 2,
+        "precio": 20
+      },{
+        "nombre": "",
+        "img": "",
+        "curacion": 3,
+        "precio": 30
+      }]
+    }, {
+      "moneda": {
+        "nombre": "",
+        "img": ""
+      }
+    }, {
+      "botas": {
+        "nombre": "",
+        "img": ""
+      }
+    }],
+    "cofre": [{
+      "img": "",
+      "objetos": []
+    }, {
+      "img": "",
+      "objetos": []
+    }]
+  });
+
+  //Subimos el json de configuración
+  $.ajax({
+    type: 'POST',
+    url: url,
+    data: {json: ficheroJSON},
+    statusCode: {
+     404: function () {
+       console.log('Slot ocupat');
+     }
+    },
+    success: function() {
+     console.log('JSON inicial guardado correctamente');
     }
-  };*/
-  //ajaxASYNC.request('http://puigpedros.salleurl.edu/pwi/pac4/partida.php?token=0aee8310-0212-424d-b2b2-8e7771e4982d');
-  ajaxASYNC.request('http://puigpedros.salleurl.edu/pwi/pac4/partida.php?token=0aee8310-0212-424d-b2b2-8e7771e4982d&slot=nueva');
+  });
+
  }
+
+/*
+ * Función para eliminar el fichero nuevaPartida.json en la API
+ */
+function eliminaJSON() {
+  $.ajax({
+    url: 'http://puigpedros.salleurl.edu/pwi/pac4/partida.php?token=0aee8310-0212-424d-b2b2-8e7771e4982d&slot=nueva',
+    type: 'DELETE',
+    success: function(result) {
+        console.log('JSON inicial eliminado');
+    }
+});
+}
--- src/js/seleccion.js
+++ src/js/seleccion.js
@@ -1,3 +1,5 @@
+var ficheroInicial;
+
 //Definimos las razas
 function Raza(nombre, nombrePasiva, descripcion, habilidad) {
   this.nombre = nombre;
@@ -5,27 +7,7 @@ function Raza(nombre, nombrePasiva, descripcion, habilidad) {
   this.pasiva = descripcion;
   this.habilidad = habilidad;
 }
-
-var altmer = new Raza('Altmer', 'Soberbía Altmer', 'La habilidad para las artes mágicas de los altmer no tiene rival. Es por eso que tienen +X de daño mágico inicial.', undefined);
-
-var bosmer = new Raza('Bosmer', 'Tiro de Valen', 'Los bosmer son los mejores arqueros de todo Tamriel. Antes de empezar el combate, es capaz de realizar un tiro con el arco que inflinge +X de daño verdadero al enemigo.', undefined);
-
-var dunmer = new Raza('Dunmer', 'Ira del antepasado', 'Los dunmer, nativos de Morrowind, donde se alza la Montaña Roja, un enorme volcán, no le temen al fuego. Cada X turnos, se rodean durante un turno de fuego, que inflinge +X de daño verdadero al enemigo cuándo ataca.', undefined);
-
-var orco = new Raza('Orco', 'Herrero de Orsinium', 'Los orcos son los mayores herreros de toda Tamriel. Las armas y escudos comprados en la tienda son un X% mejores.', undefined);
-
-var imperial = new Raza('Imperial', 'Astucia de Cyrodiil', 'Los imperiales, nativos de Cyrodiil, sobresalen sobre las demadas razas por su astucia. Por eso, son capaces de conseguir un x% más de oro.', undefined);
-
-var nordico = new Raza('Nordico', 'Fuerza de Skyrim', 'Los nordicos son una de las grandes razas guerreras de Tamriel. Su excelente habilidad para con la espada, les otorga un X% más de daño si llevan equipados una espada y un escudo.', undefined);
-
-var breton = new Raza('Breton', 'Piel de dragon', 'Los bretones, habitantes de Roca Alta, son una raza mestiza entre altmer y humanos, tienen parte de la habilidad mágica de los altmer y una gran resistencia hacia la magia. Disponen de +X de resistencia mágica y +Y de daño mágico adicional.', undefined);
-
-var guardiaRojo = new Raza('Guardia Rojo', 'Subida de adrenalina', 'Los guardia rojo, habitantes de Páramo del Martillo, son la mejor raza guerrera de toda Tamriel. Su gran envergadura les permite usar armaduras pesadas, por lo tanto, tienen +X de armadura adicional, y su execelente habilidad con armas de gran envergadura, les otorga +X de daño físico si no llevan escudo equipado.', undefined);
-
-var argoniano = new Raza('Argoniano', 'Piel de hist', 'Los argonianos, una raza con aspecto de reptiliano que habitan en la Ciénaga Negra son resistentes a casi cualquier enfermedad. Despues de cada combate, tienen un X% de curarse +Y de vida.', undefined);
-
-var khajita = new Raza('Khajita', 'Sigilo', 'Los khajita son una raza de aspecto felino originaria de Elsweyr y conocida por ser excelentes ladrones, gracias a su sigilo y agilidad. Tienen un X% de posibilidades de esquivar el ataque de un enemigo.', undefined);
-
+var altmer, bosmer, dunmer, orco, imperial, nordico, breton, guardiaRojo, argoniano, khajita;
 
 //Definimos los roles
 function Rol(nombre, descripcion, ataque, armadura, resistencia) {
@@ -35,20 +17,49 @@ function Rol(nombre, descripcion, ataque, armadura, resistencia) {
   this.armadura = armadura;
   this.resistencia = resistencia;
 }
-
-var mago = new Rol('Colegio de Hibernalia', 'En el Colegio de Hibernalia se aprende todo lo relacionado con las artes arcanas. Sus miembros son hábiles magos. +X de daño mágico adicional y -Y de armadura y resistencia mágica.', 3, -1, -1);
-
-var asesino = new Rol('Hermandad Oscura', 'La Hermandad Oscura es el gremio de asesinos más mortífero de toda Tamriel. Sus miembros son despiadados asesinos. +X de daño física adicional y -Y de armadura de resistencia mágica', 3, -1, -1);
-
-var tanque = new Rol('Legión Imperial', 'En el ejercito del imperio, la Legión Imperial, se encuentran los mejores soldados del imperio. Sus miembros son resistentes gracias a sus excelentes armaduras. -X de daño y +Y de armadura y resistencia mágica.', -2, 2, 2);
+var mago, asesino, tanque;
 
 //Seleccion de personaje
 var playerJSON;
 
 window.onload = function() {
-  //Cargamos la información inicial (altmer y mago)
-  cambioRaza();
-  cambioRol();
+  //Leemos el fichero inicial
+  var urlGet = 'http://puigpedros.salleurl.edu/pwi/pac4/partida.php?token=0aee8310-0212-424d-b2b2-8e7771e4982d&slot=nueva';
+  $.get(urlGet, function (data) {
+    var ficheroInicial = JSON.parse(data);
+
+    //Creamos las razas
+    altmer = new Raza(ficheroInicial.razas[0].nombre, ficheroInicial.razas[0].habilidad, ficheroInicial.razas[0].descripcion, undefined);
+
+    bosmer = new Raza(ficheroInicial.razas[1].nombre, ficheroInicial.razas[1].habilidad, ficheroInicial.razas[1].descripcion, undefined);
+
+    dunmer = new Raza(ficheroInicial.razas[2].nombre, ficheroInicial.razas[2].habilidad, ficheroInicial.razas[2].descripcion, undefined);
+
+    orco = new Raza(ficheroInicial.razas[3].nombre, ficheroInicial.razas[3].habilidad, ficheroInicial.razas[3].descripcion, undefined);
+
+    imperial = new Raza(ficheroInicial.razas[4].nombre, ficheroInicial.razas[4].habilidad, ficheroInicial.razas[4].descripcion, undefined);
+
+    nordico = new Raza(ficheroInicial.razas[5].nombre, ficheroInicial.razas[5].habilidad, ficheroInicial.razas[5].descripcion, undefined);
+
+    breton = new Raza(ficheroInicial.razas[6].nombre, ficheroInicial.razas[6].habilidad, ficheroInicial.razas[6].descripcion, undefined);
+
+    guardiaRojo = new Raza(ficheroInicial.razas[7].nombre, ficheroInicial.razas[7].habilidad, ficheroInicial.razas[7].descripcion, undefined);
+
+    argoniano = new Raza(ficheroInicial.razas[8].nombre, ficheroInicial.razas[8].habilidad, ficheroInicial.razas[8].descripcion, undefined);
+
+    khajita = new Raza(ficheroInicial.razas[9].nombre, ficheroInicial.razas[9].habilidad, ficheroInicial.razas[9].descripcion, undefined);
+
+    //Creamos los roles
+    mago = new Rol(ficheroInicial.roles[0].nombre, ficheroInicial.roles[0].descripcion, ficheroInicial.roles[0].ataque, ficheroInicial.roles[0].armadura, ficheroInicial.roles[0].resistenciaMagica);
+
+    asesino = new Rol(ficheroInicial.roles[1].nombre, ficheroInicial.roles[1].descripcion, ficheroInicial.roles[1].ataque, ficheroInicial.roles[1].armadura, ficheroInicial.roles[1].resistenciaMagica);
+
+    tanque = new Rol(ficheroInicial.roles[2].nombre, ficheroInicial.roles[2].descripcion, ficheroInicial.roles[2].ataque, ficheroInicial.roles[2].armadura, ficheroInicial.roles[2].resistenciaMagica);
+
+    //Cargamos la información inicial (altmer y mago)
+    cambioRaza();
+    cambioRol();
+  });
 
   //Creamos los eventos
   $('#raza').change(function() {
@@ -68,81 +79,81 @@ function creaPersonaje() {
 
   //Comprobamos que el nombre sea valido
   if ((/^\w{3,12}$/).test(nombre)) {
-    //Leemos el JSON de nueva partida
-    var nuevoJugador;
-
     console.log(nuevoJugador);
     //Actualizamos los valores del jugador en función de su raza y rol
     var ataque, tipoAtaque, armadura, resistenciaMagica;
 
-    /*switch (rol) {
+    switch (rol) {
       case 'mago':
         tipoAtaque = 'AP';
-        ataque = nuevoJugador.get('player').get('ataque') + 3;
-        armadura = nuevoJugador.get('player').get('armadura') - 1;
-        resistenciaMagica = nuevoJugador.get('player').get('resistenciaMagica') - 1;
+        ataque = nuevoJugador.ataque + mago.ataque;
+        armadura = nuevoJugador.armadura + mago.armadura;
+        resistenciaMagica = nuevoJugador.resistenciaMagica + mago.resistencia;
         break;
       case 'asesino':
         tipoAtaque = 'AD';
-        ataque = nuevoJugador.get('player').get('ataque') + 3;
-        armadura = nuevoJugador.get('player').get('armadura') - 1;
-        resistenciaMagica = nuevoJugador.get('player').get('resistenciaMagica') - 1;
+        ataque = nuevoJugador.ataque + asesino.ataque;
+        armadura = nuevoJugador.armadura + asesino.armadura;
+        resistenciaMagica = nuevoJugador.resistenciaMagica + asesino.resistencia;
         break;
       case 'tanque':
         tipoAtaque = 'AD';
-        ataque = nuevoJugador.get('player').get('ataque') - 2;
-        armadura = nuevoJugador.get('player').get('armadura') + 2;
-        resistenciaMagica = nuevoJugador.get('player').get('resistenciaMagica') + 2;
+        ataque = nuevoJugador.ataque + tanque.ataque;
+        armadura = nuevoJugador.armadura + tanque.armadura;
+        resistenciaMagica = nuevoJugador.resistenciaMagica + tanque.resistencia;
         break;
-    }*/
+    }
 
     switch (raza) {
       case 'altmer':
         if (tipoAtaque == 'AP') {
-          ataque = nuevoJugador.get('player').get('ataque') + 2;
+          ataque = ataque + 2;
         }
-        console.log('patata');
         //Creamos la habilidad
         altmer.habilidad = function () {
           console.log("ATAQUEEEE ALTMER");
         };
-        console.log('patata2');
         break;
       case 'bosmer':
         //Creamos la habilidad
         bosmer.habilidad = function () {
+          // Hace 1 de daño
           console.log("ATAQUEEEE BOSMER");
         };
         break;
       case 'dunmer':
         //Creamos la habilidad
         dunmer.habilidad = function () {
+          // Hace 4 de daño --> se carga cada 10 turnos
           console.log("ATAQUEEEE DUNMER");
         };
         break;
       case 'orco':
         //Creamos la habilidad
         orco.habilidad = function () {
+          // 20% mejores
           console.log("ATAQUEEEE ORCO");
         };
         break;
       case 'imperial':
         //Creamos la habilidad
         imperial.habilidad = function () {
+          // 25% de oro
           console.log("ATAQUEEEE IMPERIAL");
         };
         break;
       case 'nordico':
         //Creamos la habilidad
         nordico.habilidad = function () {
+          // 20% de daño extra
           console.log("ATAQUEEEE NORDICO");
         };
         break;
       case 'breton':
         if (tipoAtaque == 'AP') {
-          ataque = nuevoJugador.get('player').get('ataque') + 1;
+          ataque = ataque + 1;
         }
-        resistenciaMagica = nuevoJugador.get('player').get('resistenciaMagica') + 2;
+        resistenciaMagica = resistenciaMagica + 2;
 
         //Creamos la habilidad
         breton.habilidad = function () {
@@ -150,28 +161,31 @@ function creaPersonaje() {
         };
         break;
       case 'guardiaRojo':
-        armadura = nuevoJugador.get('player').get('armadura') + 2;
+        armadura = armadura + 2;
 
         //Creamos la habilidad
         guardiaRojo.habilidad = function () {
+          // +3 daño si no lleva escudo
           console.log("ATAQUEEEE ROJO");
         };
         break;
       case 'argoniano':
         //Creamos la habilidad
         argoniano.habilidad = function () {
+          // 33% de prob de curarse 25% de la vida restante
           console.log("ATAQUEEEE ARGONIANO");
         };
         break;
       case 'khajita':
         //Creamos la habilidad
         khajita.habilidad = function () {
+          //10% prob de esquivar
           console.log("ATAQUEEEE KHAJITA");
         };
         break;
     }
     //Creamos un JSON con la nueva información
-    playerJSON = JSON.stringify({nombre: 'Joan', raza: 'Humano', sexo: 'masculino', rol: 'ninguno', vida: 10, nivel: 0, xp: 100, ataque: 2, tipoAtaque: 'aa', armadura: 2, resistenciaMagica: 2, manoderecha: 'boli', manoizquierda: '', mochila: [], estadoPartida: {x: 3, y: 1, nivel: -2, direccion: 0}});
+    playerJSON = JSON.stringify({nombre: nombre, raza: raza, sexo: sexo, rol: rol, vida: 10, nivel: 0, xp: 0, ataque: ataque, tipoAtaque: tipoAtaque, armadura: armadura, resistenciaMagica: resistenciaMagica, manoderecha: 'boli', manoizquierda: '', mochila: [], estadoPartida: {x: 3, y: 1, nivel: -2, direccion: 0}});
 
     console.log(playerJSON);
     //Avanzamos hasta el juego
